8c7d82e13f42d1fc69707be987993aa231a566fa
Joan Antoni Orfila <jtorfila@gmail.com>
2021-07-06 14:57:52 +0200

    Reactivo FormArray

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+
+    this.crearFormulario();
+    this.cargarDatosAlFormulario();
+
+  }
+
+  ngOnInit(): void { }
+
+  get pasatiempos () {
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
+  crearFormulario() {
+
+    this.forma = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', [Validators.required, Validators.minLength(5)]],
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required],
+      }), pasatiempo: this.fb.array([
+        [], [], [], []
+      ])
+    });
+
+  }
+
+  cargarDatosAlFormulario() {
+
+    this.forma.setValue({
+      nombre: 'Benito',
+      apellido: 'Camela',
+      correo: 'benito@camela.ya',
+      direccion: {
+        distrito: 'Ontario',
+        ciudad: 'Otawa'
+      }
+    });
+
+  }
+
+  guardar() {
+    console.log(this.forma);
+
+    if (this.forma.invalid) {
+
+      return Object.values(this.forma.controls).forEach(control => {
+        control.markAllAsTouched();
+      })
+
+    }
+
+    // POST de info
+    this.forma.reset({
+      
+    })
+
   }
 
 }
